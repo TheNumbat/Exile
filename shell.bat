2535045d715b66af3d2ea748219dc349f24c1e50
TheNumbat <mslater@nevada.unr.edu>
2017-04-18 11:06:28 -0700

    etup

--- shell.bat
+++ shell.bat
@@ -3,7 +3,7 @@
 IF NOT EXIST w: subst w: "%cd%"
 w:
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set path=%cd%bin;%path%
 
 cls
