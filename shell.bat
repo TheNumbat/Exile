77926cb7a11c5e372f2753a9d774a0f8c2eba0c6
Maxwell Slater <mslater@nevada.unr.edu>
2017-05-21 21:12:08 -0700

    more shell

--- shell.bat
+++ shell.bat
@@ -4,7 +4,10 @@ IF EXIST w: subst /d w:
 subst w: "%cd%"
 w:
 
-IF NOT EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\" echo "You don't have visual studio!"
-IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\" call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+IF EXIST "D:\VBS\VC\Auxiliary\Build" (
+	call "D:\VBS\VC\Auxiliary\Build\vcvarsall.bat" x64 
+) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 
+)
 
 set path=%cd%bin;%path%
