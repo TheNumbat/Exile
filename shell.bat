5699d7b9bbb045670e9c6c55209de17b7f6463a6
TheNumbat <mslater@nevada.unr.edu>
2017-06-19 13:40:34 -0700

    update shell.bat

--- shell.bat
+++ shell.bat
@@ -4,12 +4,14 @@ IF EXIST w: subst /d w:
 subst w: "%cd%"
 w:
 
-IF EXIST "D:\VBS\VC\Auxiliary\Build" (
-	call "D:\VBS\VC\Auxiliary\Build\vcvarsall.bat" x64 
-) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\" (
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
 	call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64 
-) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools" (
+) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\Shared\14.0\VC\vcvarsall.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio\Shared\14.0\VC\vcvarsall.bat" 
+) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat" (
 	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+) ELSE IF EXIST "D:\VBS\VC\Auxiliary\Build\vcvarsall.bat" (
+	call "D:\VBS\VC\Auxiliary\Build\vcvarsall.bat" x64 
 )
 
 set path=%cd%bin;%path%
