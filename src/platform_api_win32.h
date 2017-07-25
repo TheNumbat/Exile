c6ac9cf728378640eac301c62840c701442bdf2d
TheNumbat <mslater@nevada.unr.edu>
2017-07-25 10:08:00 -0700

    add files for last commit

---
+++ src/platform_api_win32.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <gl/gl.h>
+
+// from windowsx.h
+#define GET_WPARAM(wp, lp)                      (wp)
+#define GET_LPARAM(wp, lp)                      (lp)
+#define GET_X_LPARAM(lp)                        ((int)(short)LOWORD(lp))
+#define GET_Y_LPARAM(lp)                        ((int)(short)HIWORD(lp))
+
+// API Structures (mark opaque/transparent)
+
+u32 PLATFORM_SHARING_ERROR = 32;
+
+struct platform_window {
+	// Transparent
+	string title;
+
+	// Opaque block, do not use outside of platform code
+	WNDCLASSEX window_class				= {};
+	HWND handle							= {};
+	HDC device_context					= {};
+	PIXELFORMATDESCRIPTOR pixel_format 	= {};
+	HGLRC gl_temp					   	= {};
+	HGLRC gl_context					= {};
+};
+
+struct platform_dll {
+	// Opaque
+	HMODULE dll_handle = {};
+};
+
+struct platform_file_attributes {
+	// Opaque
+	WIN32_FILE_ATTRIBUTE_DATA attrib = {};
+};
+
+struct platform_thread_id {
+	// Transparent
+	DWORD id 	= NULL;
+};
+
+bool operator==(platform_thread_id first, platform_thread_id second) {
+	return first.id == second.id;
+}
+
+struct platform_thread {
+	// transparent
+	platform_thread_id id;
+
+	// Opaque
+	HANDLE handle 	= NULL;
+};
+
+struct platform_semaphore {
+	// Opaque
+	HANDLE handle 	= NULL;
+};
+
+struct platform_mutex {
+	// Opaque
+	HANDLE handle 	= NULL;
+};
+
+struct platform_file {
+	// Transparent
+	string path;
+
+	// Opaque
+	HANDLE handle 	= NULL;
+};
+
+bool operator==(platform_file first, platform_file second) {
+	return first.handle == second.handle;
+}
