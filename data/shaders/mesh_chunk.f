8fc3892b4d2021883cc7cba48a682036913bcc5e
TheNumbat <mslater@nevada.unr.edu>
2018-01-29 17:36:09 -0800

    add shaders

---
+++ data/shaders/mesh_chunk.f
@@ -0,0 +1,8 @@
+
+#version 450 core
+
+out vec4 color;
+
+void main() {
+	color = vec4(1.0);
+}
---
+++ data/shaders/mesh_chunk.v
@@ -0,0 +1,18 @@
+
+#version 450 core
+
+layout (location = 0) in uvec2 vertex;
+
+uniform mat4 transform;
+
+const uint l_mask = 0xff000000;
+const uint z_mask = 0x00ff0000;
+const uint y_mask = 0x0000ff00;
+const uint x_mask = 0x000000ff;
+
+void main() {
+
+	vec3 pos = vec3(vertex.x & x_mask, (vertex.x & y_mask) >> 8, (vertex.x & z_mask) >> 16);
+	
+	gl_Position = transform * vec4(pos, 1.0);
+}
