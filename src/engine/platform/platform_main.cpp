4918337e02e881c8d44e6f73b3665219a7b87e2e
TheNumbat <mslater@nevada.unr.edu>
2018-03-01 15:50:47 -0800

    fix platform layer code function prefixes

--- src/engine/platform/platform_main.cpp
+++ src/engine/platform/platform_main.cpp
@@ -60,23 +60,23 @@ int main() {
 	api.your_dll = &game_dll;
 
 	string exe_path;
-	platform_error err = api.platform_get_bin_path(&exe_path);
+	platform_error err = api.get_bin_path(&exe_path);
 	if(!err.good) {
 		return 1;
 	}
 
 	int idx = string_last_slash(exe_path);
-	exe_folder 	  = make_substring(exe_path, 0, idx + 1, api.platform_heap_alloc);
-	dll_path 	  = make_cat_string(exe_folder, string_literal("game.dll"), api.platform_heap_alloc);
-	temp_dll_path = make_cat_string(exe_folder, string_literal("game_temp.dll"), api.platform_heap_alloc);
+	exe_folder 	  = make_substring(exe_path, 0, idx + 1, api.heap_alloc);
+	dll_path 	  = make_cat_string(exe_folder, string_literal("game.dll"), api.heap_alloc);
+	temp_dll_path = make_cat_string(exe_folder, string_literal("game_temp.dll"), api.heap_alloc);
 
-	free_string(exe_path, api.platform_heap_free);
+	free_string(exe_path, api.heap_free);
 
 	if(!load_lib()) {
 		return 1;
 	}
 
-	err = api.platform_get_file_attributes(&attrib, dll_path);
+	err = api.get_file_attributes(&attrib, dll_path);
 	if(!err.good) {
 		return 1;
 	}
@@ -99,14 +99,14 @@ int main() {
 
 	(*shut_down)(game_state);
 
-	api.platform_free_library(&game_dll);
+	api.free_library(&game_dll);
 	
-	free_string(exe_folder, api.platform_heap_free);
-	free_string(dll_path, api.platform_heap_free);
-	free_string(temp_dll_path, api.platform_heap_free);
+	free_string(exe_folder, api.heap_free);
+	free_string(dll_path, api.heap_free);
+	free_string(temp_dll_path, api.heap_free);
 
 	if(global_num_allocs) {
-	 	api.platform_debug_break();	
+	 	api.debug_break();	
 	}
 
 	return 0;
@@ -119,15 +119,15 @@ bool load_lib() {
 	u32 itr = 0;
 	do {
 		itr++;
-		err = api.platform_copy_file(dll_path, temp_dll_path, true);
+		err = api.copy_file(dll_path, temp_dll_path, true);
 	} while(err.error == PLATFORM_SHARING_ERROR && itr < 100000);
 
-	err = api.platform_load_library(&game_dll, temp_dll_path);
+	err = api.load_library(&game_dll, temp_dll_path);
 	if(!err.good) {
 		return false;
 	}
 
-	err = api.platform_get_file_attributes(&attrib, dll_path);
+	err = api.get_file_attributes(&attrib, dll_path);
 	if(!err.good) {
 		return false;
 	}
@@ -139,18 +139,18 @@ bool try_reload() {
 
 	platform_file_attributes to_test;
 
-	platform_error err = api.platform_get_file_attributes(&to_test, dll_path);
+	platform_error err = api.get_file_attributes(&to_test, dll_path);
 	if(!err.good) {
 		return false;
 	}
 
-	if(api.platform_test_file_written(&attrib, &to_test)) {
+	if(api.test_file_written(&attrib, &to_test)) {
 		
 		attrib = to_test;
 
 		(*on_unload)(game_state);
 
-		api.platform_free_library(&game_dll);
+		api.free_library(&game_dll);
 
 		if(!load_lib()) return false;
 
@@ -164,27 +164,27 @@ bool try_reload() {
 
 bool load_funcs() {
 
-	platform_error err = api.platform_get_proc_address((void**)&start_up, &game_dll, string_literal("start_up"));
+	platform_error err = api.get_proc_address((void**)&start_up, &game_dll, string_literal("start_up"));
 	if(!err.good) {
 		return false;
 	}
 
-	err = api.platform_get_proc_address((void**)&main_loop, &game_dll, string_literal("main_loop"));
+	err = api.get_proc_address((void**)&main_loop, &game_dll, string_literal("main_loop"));
 	if(!err.good) {
 		return false;
 	}
 
-	err = api.platform_get_proc_address((void**)&shut_down, &game_dll, string_literal("shut_down"));
+	err = api.get_proc_address((void**)&shut_down, &game_dll, string_literal("shut_down"));
 	if(!err.good) {
 		return false;
 	}
 
-	err = api.platform_get_proc_address((void**)&on_reload, &game_dll, string_literal("on_reload"));
+	err = api.get_proc_address((void**)&on_reload, &game_dll, string_literal("on_reload"));
 	if(!err.good) {
 		return false;
 	}
 
-	err = api.platform_get_proc_address((void**)&on_unload, &game_dll, string_literal("on_unload"));
+	err = api.get_proc_address((void**)&on_unload, &game_dll, string_literal("on_unload"));
 	if(!err.good) {
 		return false;
 	}
