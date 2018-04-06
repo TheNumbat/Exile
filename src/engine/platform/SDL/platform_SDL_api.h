1459d1bf23975cab1df4eebe2c46bb2bc9205cd4
TheNumbat <mslater@nevada.unr.edu>
2018-04-06 13:07:59 -0700

    add files

---
+++ src/engine/platform/SDL/platform_SDL_api.h
@@ -0,0 +1,55 @@
+
+#include <SDL2/SDL.h>
+
+/* Included in platform_api.h */
+
+u32 PLATFORM_SHARING_ERROR = 0;
+
+struct platform_window {
+	// Transparent
+	string title;
+	i32 w = 0, h = 0;
+	SDL_Window* window;
+
+	// Opaque block, do not use outside of platform code
+};
+
+struct platform_dll {
+	// Opaque
+	
+};
+
+struct platform_file_attributes {
+	// Opaque
+	
+};
+
+struct platform_thread {
+	// transparent
+	platform_thread_id id;
+
+	// Opaque
+	
+};
+
+struct platform_semaphore {
+	// Opaque
+	
+};
+
+struct platform_mutex {
+	// Opaque
+	
+};
+
+struct platform_file {
+	// Transparent
+	string path;
+
+	// Opaque
+	
+};
+
+bool operator==(platform_file first, platform_file second) {
+	return false;
+}
