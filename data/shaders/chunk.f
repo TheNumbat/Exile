0bce43bcc7a49c66ee3776e5ec6989c02b03615c
TheNumbat <mslater@nevada.unr.edu>
2018-08-24 22:37:46 -0400

    dist

--- data/shaders/chunk.f
+++ data/shaders/chunk.f
@@ -39,7 +39,7 @@ void main() {
 
 	if(do_fog) {
 
-		float fog_factor = clamp(f_d / (1.25f * render_distance), 0.0f, 1.0f);
+		float fog_factor = clamp(f_d / render_distance, 0.0f, 1.0f);
 
 		vec3 sky_color = texture(sky_tex, vec2(day_01, f_ah)).rgb;
 
