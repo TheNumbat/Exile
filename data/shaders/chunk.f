3f5c9100966ad96027a324e5f63f315a8e619f0a
TheNumbat <mslater@nevada.unr.edu>
2018-10-26 13:24:09 -0400

    fog curve

--- data/shaders/chunk.f
+++ data/shaders/chunk.f
@@ -39,7 +39,7 @@ void main() {
 
 	if(do_fog) {
 
-		float fog_factor = pow(clamp(f_d / render_distance - 0.05f, 0.0f, 1.0f), 2);
+		float fog_factor = pow(clamp(f_d / render_distance - 0.05f, 0.0f, 1.0f), 3);
 		vec3 sky_color = texture(sky_tex, vec2(day_01, f_ah)).rgb;
 
 		color = mix(color, sky_color, fog_factor);
