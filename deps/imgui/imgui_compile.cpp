75fe025be78842457dafaa844fe5de50e33844fb
TheNumbat <mslater@nevada.unr.edu>
2019-10-18 20:10:56 -0400

    more organization

---
+++ deps/imgui/imgui_compile.cpp
@@ -0,0 +1,6 @@
+
+#include "imgui.cpp"
+#include "imgui_draw.cpp"
+#include "imgui_widgets.cpp"
+#include "imgui_impl_sdl.cpp"
+#include "imgui_impl_opengl3.cpp"
--- src/lib/alloc_after.h
+++ src/lib/alloc_after.h
@@ -1,6 +1,20 @@
 
 #pragma once
 
+#ifdef _WIN32
+    #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+    #endif
+    #include <windows.h>
+#elif defined(__linux__)
+    #include <sys/mman.h>
+#endif
+
+#include <atomic>
+
+inline std::atomic<i64> allocs = 0;
+inline std::atomic<i64> vallocs = 0;
+
 template<const char* tname, usize N>
 template<typename T>
 T* MSarena<tname, N>::alloc(usize size, usize align) {
@@ -26,4 +40,47 @@ T* MVarena<tname, N>::alloc(usize size, usize align) {
     used += offset + size;
     high_water = _MAX(high_water, used);
     return ret;
+}
+
+inline void* base_alloc(usize sz) {
+    void* ret = calloc(sz, 1);
+    assert(ret);
+    allocs++;
+    return ret;
+}
+
+inline void base_free(void* mem) {
+    if(!mem) return;
+    allocs--;
+    free(mem);
+}
+
+inline void* virt_alloc(usize sz) {
+    vallocs++;
+#ifdef _WIN32
+    void* ret = VirtualAlloc(null, sz, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+    assert(ret);
+#elif defined(__linux__) 
+    void* ret = mmap(null, sz, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    assert(ret != MAP_FAILED);
+#endif
+    return ret;
+}
+
+inline void virt_free(void* mem) {
+    if(!mem) return;
+    vallocs--;
+#ifdef _WIN32
+    VirtualFree(mem, 0, MEM_RELEASE);
+#elif defined(__linux__) 
+    munmap(mem, 0);
+#endif
+}
+
+inline void mem_validate() {
+    if(allocs != 0 || vallocs != 0) {
+        warn("Unbalanced allocations: %, (vert) %", allocs.load(), vallocs.load());
+    } else {
+        info("No memory leaked.");
+    }
 }
