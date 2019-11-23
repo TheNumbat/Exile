abad69f4da0b2fd2f147728b41009b64496e6f92
TheNumbat <mslater@nevada.unr.edu>
2019-11-23 16:24:31 -0500

    support stop profiling

--- src/gui/dbg_gui.cpp
+++ src/gui/dbg_gui.cpp
@@ -13,28 +13,32 @@ void Dbg_Gui::destroy() {
 
 void Dbg_Gui::profiler() {
 
-    map<Location,Graph_Entry,Mframe> accum;
-    
-    Profiler::iterate_timings([&accum](Profiler::thread_id id, Profiler::Timing_Node n) {
-        if(id != std::this_thread::get_id()) return;
-        Graph_Entry& t = accum.get_or_insert(n.loc);
-        t.name = n.loc.func;
-        t.endTime += Profiler::ms(n.self_time);
-    });
+    if(!prof_window.stopProfiling) {
 
-    vec<Graph_Entry,Mframe> data = vec<Graph_Entry,Mframe>::make(accum.size);
-    
-    for(auto& t : accum) data.push(t.value);
-    
-    std::sort(data.begin(), data.end(), [](auto l, auto r) {
-        return l.endTime < r.endTime;
-    });
+        map<Location,Graph_Entry,Mframe> accum;
+        
+        Profiler::iterate_timings([&accum](Profiler::thread_id id, Profiler::Timing_Node n) {
+            if(id != std::this_thread::get_id()) return;
+            Graph_Entry& t = accum.get_or_insert(n.loc);
+            t.name = n.loc.func;
+            t.endTime += Profiler::ms(n.self_time);
+        });
 
-    for(u32 i = 1; i < data.size; i++) {
-        data[i].startTime = data[i-1].endTime;
-        data[i].endTime += data[i].startTime;
+        vec<Graph_Entry,Mframe> data = vec<Graph_Entry,Mframe>::make(accum.size);
+        
+        for(auto& t : accum) data.push(t.value);
+        
+        std::sort(data.begin(), data.end(), [](auto l, auto r) {
+            return l.endTime < r.endTime;
+        });
+
+        for(u32 i = 1; i < data.size; i++) {
+            data[i].startTime = data[i-1].endTime;
+            data[i].endTime += data[i].startTime;
+        }
+
+        prof_window.cpuGraph.load_frame_data(data.data, data.size);
     }
-
-    prof_window.cpuGraph.load_frame_data(data.data, data.size);
+    
     prof_window.render();
 }
